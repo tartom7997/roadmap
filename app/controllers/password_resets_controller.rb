6b703e347dbba4043aab3cf4cb46abd4b315be8a
tartom7997 <tartom7997gmail.com>
2021-01-11 23:04:32 +0900

    Add password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,67 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]    # (1) への対応：パスワード再設定の有効期限が切れていないか
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "パスワードの再設定のためのEメールが送られました。"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Eメールアドレスが見つかりませんでした。"
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].empty?                  # (3) への対応 新しいパスワードが空文字列になっていないか (ユーザー情報の編集ではOKだった)
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update(user_params)          # (4) への対応 新しいパスワードが正しければ、更新する
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "パスワードは再設定されました。."
+      redirect_to @user
+    else
+      render 'edit'                                     # (2) への対応 無効なパスワードであれば失敗させる (失敗した理由も表示する)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # beforeフィルタ
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    # トークンが期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "パスワードの再設定は有効期限切れです。"
+        redirect_to new_password_reset_url
+      end
+    end
+
+end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,12 +5,23 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:michael)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "アカウントの有効化について", mail.subject
+    assert_equal "【Roadmap】アカウントの有効化について", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
     assert_match user.name,               mail.text_part.body.to_s.encode("UTF-8")
     assert_match user.activation_token,   mail.text_part.body.to_s.encode("UTF-8")
     assert_match CGI.escape(user.email),  mail.text_part.body.to_s.encode("UTF-8")
     # https://hmng117.hatenablog.com/entry/2019/01/08/191000
+  end
+
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "【Roadmap】パスワードの再設定について", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.text_part.body.to_s.encode("UTF-8")
+    assert_match CGI.escape(user.email),  mail.text_part.body.to_s.encode("UTF-8")
   end
 end
