4e0c190601e1bb1d8d31e498cda936775bcd6ba0
tartom7997 <tartom7997@gmail.com>
2021-05-01 15:21:02 +0900

    front fix 210501

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :detect_device
 
   private
 
@@ -11,4 +12,18 @@ class ApplicationController < ActionController::Base
         redirect_to login_url
       end
     end
+
+    def detect_device
+      case request.user_agent
+        when /iPad/
+            request.variant = :mobile
+        when /iPod/
+            request.variant = :mobile
+        when /iPhone/
+            request.variant = :mobile
+        when /Android/
+            request.variant = :mobile
+      end
+    end
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,9 @@ require("jquery")
 
 // 追加
 require ('channels/icon_preview.js')
+require ('channels/count.js')
+require ('channels/count_50.js')
+require ('channels/count_100.js')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
