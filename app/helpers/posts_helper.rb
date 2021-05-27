5c2d3680e722a5f8f698f13cf5bc16e2d22e4e4b
tartom7997 <tartom7997@gmail.com>
2021-05-27 23:46:22 +0900

    hashtag function2

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -15,7 +15,9 @@ module PostsHelper
       end
 
       def render_with_hashtags_post(hashbody)
-        hashbody.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/) { |word| link_to word, "/post/hashtag/#{word.delete("#")}",data: {"turbolinks" => false} }.html_safe
+        if hashbody.present?
+          hashbody.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/) { |word| link_to word, "/post/hashtag/#{word.delete("#")}",data: {"turbolinks" => false} }.html_safe
+        end
       end
 
 end
--- app/helpers/roadmaps_helper.rb
+++ app/helpers/roadmaps_helper.rb
@@ -1,7 +1,9 @@
 module RoadmapsHelper
 
     def render_with_hashtags_roadmap(hashbody)
+      if hashbody.present?
         hashbody.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/) { |word| link_to word, "/roadmap/hashtag/#{word.delete("#")}",data: {"turbolinks" => false} }.html_safe
       end
+    end
 
 end
