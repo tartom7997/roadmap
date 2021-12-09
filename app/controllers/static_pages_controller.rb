849a2ad58cfaeb0865985b6b6bd3bd63309d419d
tartom7997 <tartom7997@gmail.com>
2021-12-09 15:35:04 +0900

    toppagefix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,8 +9,11 @@ class StaticPagesController < ApplicationController
       @roadmap_first  = current_user.roadmaps.order(updated_at: :desc).first
       @post = current_user.feed_post.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
       # @feed_post_items = current_user.feed_post.paginate(page: params[:page]).order(created_at: :desc)
-    # else
-    #   @roadmaps = Roadmap.includes(:like_roadmaps, :steps => :posts).where(like_roadmaps: { created_at: 0.days.ago.beginning_of_month..0.days.ago.end_of_month}).group(:id).order(Arel.sql("count(*) desc")).limit(5)
+    else
+      @roadmaps = Roadmap.includes(:like_roadmaps, :steps => :posts).where(
+        like_roadmaps: { 
+          created_at: Time.now.all_month
+        }).limit(3).sort {|a,b| b.liking_users.size <=> a.liking_users.size}
     end
   end
 
